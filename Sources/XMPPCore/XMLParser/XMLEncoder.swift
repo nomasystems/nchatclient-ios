961188e8b740f469992b9799f1b8c5f3d12ed6ef
Alberto Caamaño Souto <albertocsouto@gmail.com>
2022-09-26 15:13:22 +0200

    Fixed encoding for single and double quotes. Closes #3 (#4)

--- Sources/XMPPCore/XMLParser/XMLEncoder.swift
+++ Sources/XMPPCore/XMLParser/XMLEncoder.swift
@@ -63,6 +63,7 @@ class XMLEncoder {
         return str.replacingOccurrences(of: "&", with: "&amp;")
             .replacingOccurrences(of: "<", with: "&lt;")
             .replacingOccurrences(of: ">", with: "&gt;")
-            .replacingOccurrences(of: "'", with: "&quot;")
+            .replacingOccurrences(of: "\"", with: "&quot;")
+            .replacingOccurrences(of: "'", with: "&apos;")
     }
 }
